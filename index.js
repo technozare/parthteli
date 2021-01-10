985fa2725c173b716d62b9d24d6bd8a957eae0b5
technozare <71579763+technozare@users.noreply.github.com>
2021-01-10 13:36:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const sr = ScrollReveal();
+
+sr.reveal('.textBox h1', {
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.about-me-container img', {
+    delay: 400,
+    duration: 300,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.about-me-container p', {
+    delay: 400,
+    duration: 600,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.projects-container h3', {
+    delay: 400,
+    duration: 800,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.projects-container #snapshot-1', {
+    delay: 400,
+    duration: 800,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.projects-container #snapshot-2', {
+    delay: 400,
+    duration: 800,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.projects-container #snapshot-3', {
+    delay: 400,
+    duration: 800,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.projects-container #snapshot-4', {
+    delay: 400,
+    duration: 800,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.projects-container #snapshot-1', {
+    delay: 400,
+    duration: 800,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.review-container img', {
+    delay: 400,
+    duration: 300,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.review-container p', {
+    delay: 400,
+    duration: 600,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+
+
+
+
